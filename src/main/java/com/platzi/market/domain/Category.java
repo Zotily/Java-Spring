1289f5b204f35926ef95f4904784ad908c4f6a94
Alex Treviño <alex_zotily@hotmail.com>
2023-10-15 19:18:56 -0600

    Repository, Service and Mapper classes implemented

---
+++ src/main/java/com/platzi/market/domain/Category.java
@@ -0,0 +1,31 @@
+package com.platzi.market.domain;
+
+public class Category {
+    private long categoryId;
+    private String category;
+    private Boolean active;
+
+    public long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
